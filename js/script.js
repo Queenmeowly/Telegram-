13e978d15daa63e82e69dd98cd8169c6463bae61
Queenmeowly <meowhoyo@gmail.com>
2026-06-21 03:12:55 +0330

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,49 @@
-let coins=0
+let coins=
 
-let power=1
+Number(
+localStorage.coins||0
+)
+
+let power=
+
+Number(
+localStorage.power||1
+)
+
+let powerLevel=
+
+Number(
+localStorage.level||1
+)
+
+let powerPrice=
+
+Number(
+localStorage.price||10
+)
+
+
+
+let last=
+
+Number(
+localStorage.last||Date.now()
+)
+
+
+
+// ماین آفلاین
+
+let passed=
+
+Math.floor(
+(
+Date.now()-last
+)
+/10000
+)
+
+coins+=passed
 
 
 
@@ -10,6 +53,8 @@ document.getElementById(
 "tap"
 )
 
+
+
 let coinText=
 
 document.getElementById(
@@ -18,12 +63,141 @@ document.getElementById(
 
 
 
+let levelText=
+
+document.getElementById(
+"powerLevel"
+)
+
+
+
+let priceText=
+
+document.getElementById(
+"powerPrice"
+)
+
+
+
 tap.onclick=()=>{
 
 coins+=power
+
+
+tap.classList.add(
+"pop"
+)
+
+
+setTimeout(()=>{
+
+tap.classList.remove(
+"pop"
+)
+
+},80)
+
+
+
+save()
+
+render()
+
+}
+
+
+
+document
+.getElementById(
+"powerBtn"
+)
+
+.onclick=()=>{
+
+
+if(
+coins<
+powerPrice
+)
+
+return
+
+
+coins-=
+
+powerPrice
+
+
+power++
+
+powerLevel++
+
+
+// گرون شدن
+
+powerPrice=
+
+Math.floor(
+powerPrice*2.2
+)
+
+
+save()
+
+render()
+
+}
+
+
+
+function render(){
 
 coinText.innerText=
 
 coins
 
+
+levelText.innerText=
+
+powerLevel
+
+
+priceText.innerText=
+
+powerPrice
+
 }
+
+
+
+function save(){
+
+localStorage.coins=
+
+coins
+
+
+localStorage.power=
+
+power
+
+
+localStorage.level=
+
+powerLevel
+
+
+localStorage.price=
+
+powerPrice
+
+
+localStorage.last=
+
+Date.now()
+
+}
+
+
+
+render()
